06b24e224c7378d77642a13780aefc668cf149fa
Chez-Guy <jack.sciortino@outlook.com>
2025-07-03 12:09:48 -0400

    Refactor configuration handling to load from config.json, update README for clarity, and move db_structure.sql file to templates folder.

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # Botitoo, the Discord Bot 🤖
 Botitoo is a Discord bot built on [Discord.py](https://github.com/Rapptz/discord.py) and uses a [PostgreSQL](https://www.postgresql.org/) database. This bot is intended for use in the [Chezito's Cavern](https://discord.gg/chez) Discord server.
 
-I've provided the packages this bot requires in the [requirements.txt](https://github.com/Chez-Guy/Botitoo/blob/main/requirements.txt) as well as the database template in [db_structure.sql](https://github.com/Chez-Guy/Botitoo/blob/main/db_structure.sql). Not *entirely* sure if the `db_structures.sql` will work when trying to import into a PostgreSQL database, but the structure and everything you'll need to set it up is in there, so you can look at it for reference. 🙂 If you do decide to pull the source code and put it in your own server, all of the variables that need to be changed for your own server are on lines 37 thru 51 in the main file [bot.py](https://github.com/Chez-Guy/Botitoo/blob/main/botitoo/bot.py).
-
-As always, if any issues arrise using it in your own server, please post them in the [Issues](https://github.com/Chez-Guy/Botitoo/issues) tab, and I'll do my best to get on it!
+This is my first time making a project of mine open source and giving others the ability to clone it to their own instance, so if any issues arrise using it in your own server, please post them in the [Issues](https://github.com/Chez-Guy/Botitoo/issues) tab, and I'll do my best to get on it!
 
 ## Features
 ### Commands:
@@ -12,3 +10,25 @@ As always, if any issues arrise using it in your own server, please post them in
 - `/get_count` - See what the current count is in the VIP-only counting channel
 - `/set_count` - For mods to set the count of the VIP-only counting channel in case the bot goes offline and/or is inaccurate
 - `/register_role` - For mods to link custom role subscribers to their custom role, so when they unsubscribe, their role will be revoked
+
+## Instructions
+* Download [Python 3.11.12](https://www.python.org/downloads/release/python-31112/)
+* Change to `botitoo/` directory 
+* Create a Virtual Environment
+```python -m venv .venv```
+* Activate Virutal Environment
+> * **For Windows (Command Prompt or PowerShell):**
+> ```.venv\Scripts\activate```
+> * **For macOS / Linux:**
+> ```source .venv/bin/activate```
+* Install packages 
+```pip install -r requirements.txt```
+* Create an `.env` file and add your Discord bot `TOKEN`, your PostgreSQL `DB_HOST`, `DB_USER`, `DB_PASS`, and `DB_DATA` 
+* Rename `config.json.template` to `config.json` and fill in your server-specific IDs
+* Start the bot
+> * **For process manager ([PM2](https://pm2.keymetrics.io/)):**
+> ```pm2 start botitoo-pm2.json```
+> * **Python:**
+> ```python main.py```
+
+Database template is located in [db_structure.sql](https://github.com/Chez-Guy/Botitoo/blob/main/templates/db_structure.sql). Not *entirely* sure if the `db_structures.sql` will work if you try to import into a PostgreSQL database editor, but the structure and everything you'll need to set it up is in that file, so you can look at it for reference and try to copy it as best you can. 🙂 I'm sure any version PostgreSQL will work just fine.
--- botitoo/bot.py
+++ botitoo/bot.py
@@ -1,8 +1,7 @@
 import os
+import json
 import discord
-from discord import app_commands
 from discord.ext import commands
-from datetime import datetime, date
 from dotenv import dotenv_values
 import glob
 import psycopg
@@ -25,6 +24,13 @@ class Botitoo(commands.Bot):
         
         self.env = dotenv_values("../botitoo/.env")
 
+        try:
+          with open('../botitoo/config.json') as f:
+            self.config = json.load(f)
+        except FileNotFoundError:
+          print("Error: config.json not found. Please create it from config.json.template.")
+          exit()
+
         self.db_params= {
             "host": self.env['DB_HOST'],
             "user": self.env['DB_USER'],
@@ -34,59 +40,19 @@ class Botitoo(commands.Bot):
         self.db=psycopg.connect(**self.db_params)
         self.cursor = self.db.cursor()
 
-        """ 
-        Change the variables below to configure with your own server
-        A lot of these are based around roles created for channel members, discord subscribers, and other stuff,
-        but can be replicated into your own server by changing these IDs to your own roles/channels
-        """
-
-        self.colors = [1193322620779778146,1193322880331690024,1193322903173857321,1193322915345731604,1193322927375003708,1193322940603838525,1199434340770267257,1204570321433272320] 
-    
-        self.guildID = 1128048701387055209
-
-        self.general_chatID = 1128048705363251265
-
-        self.shoutout_channelID = 1194447194137297129
-
-        self.count_channelID = 1136238325825536070
-
-        self.custom_role_subscriberID = 1345992570601209890
-
-        self.server_log_channelID = 1321148137494020157
-
-        self.booster_roleID = 1141138598880620627
-
-        self.youtube_memberID = 1138520134118559884
+        self.colors = [int(id) for id in self.config.get("colors")] # usually "colors" is an array, might mess up if it's not an array
+        self.guildID = int(self.config.get("guildID"))
+        self.general_chatID = int(self.config.get("general_chatID"))
+        self.shoutout_channelID = int(self.config.get("shoutout_channelID"))
+        self.count_channelID = int(self.config.get("count_channelID"))
+        self.custom_role_subscriberID = int(self.config.get("custom_role_subscriberID"))
+        self.server_log_channelID = int(self.config.get("server_log_channelID"))
+        self.booster_roleID = int(self.config.get("booster_roleID"))
+        self.youtube_memberID = int(self.config.get("youtube_memberID"))
 
         self.usedLink = None
 
         super().__init__(intents=intents, command_prefix='b!')
-
-    '''
-    # submissions or media_share_users for database
-    def changeMS_database(self, database, change, id, val=None): # id is user's id, val is how much to change by (default for val is none for funcs that want to read)
-      self.cursor.execute('SELECT id FROM '+database+' WHERE id="'+str(id)+'"')
-      if self.cursor.fetchone(): # check if usr exists in db
-        if change=="read":
-          self.cursor.execute('SELECT count FROM '+database+' WHERE id="'+str(id)+'"')
-          return self.cursor.fetchone()[0]
-        elif change=="write":
-          self.cursor.execute('UPDATE '+database+' SET count = '+str(self.changeMS_database(database, "read", id)+val)+' WHERE id = "'+str(id)+'"')
-          self.db.commit()
-      else:
-        self.cursor.execute('INSERT INTO '+database+' (id, count) VALUES ("'+str(id)+'", 0)')
-        self.db.commit()
-        return 1
-
-    #TODO: make command in discord to whitelist video/user
-    #TODO: update to check for roles to whitelist if its for users?
-    def checkWhitelist(self, database, id):
-      self.cursor.execute('SELECT whitelisted FROM '+database+' WHERE id="'+str(id)+'"')
-      result = self.cursor.fetchone()
-      if result:
-        if result[0]==1: return True 
-        elif result[0]==0: return False
-'''
     
     def addToStorage(self, name, item):
         self.cursor.execute(f"SELECT name FROM previous WHERE name = '{name}'")
@@ -105,13 +71,7 @@ class Botitoo(commands.Bot):
       self.cursor.execute(f"SELECT storage FROM previous WHERE name = '{name}'")  # apparently this isnt good but whatever. only thing that works
       result = self.cursor.fetchone()
       return result[0]
-    # (media share)
 
-
-    # TODO: add other stuff from old botitoo file
-
-
-    # end of custom functions
     async def on_connect(self):
         for filename in glob.iglob("botitoo/cogs/**", recursive=True):
             if filename.endswith(".py"):
